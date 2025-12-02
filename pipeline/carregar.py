68341507ac3f7d212ca20b93255296e0b067fc0d
pedromelo <pedro.feitoza@gmail.com>
2025-12-01 21:06:19 -0300

    Carregamento dos dados do arquivo csv

--- pipeline/carregar.py
+++ pipeline/carregar.py
@@ -1,27 +1,27 @@
 """
 Pipeline - Etapa 1: Carregar e Explorar Dados
 """
-
+from pathlib import Path
 import pandas as pd
 
 
 def carregar_dados(caminho_arquivo):
     """
     Carrega o dataset de clientes.
-    
-    Args:
-        caminho_arquivo: caminho para o CSV
-        
-    Returns:
-        DataFrame com os dados
     """
+    # Args:
+    # caminho_arquivo: caminho para o CSV
+    BASE_DIR = Path(__file__).parent.parent 
+    caminho_arquivo = BASE_DIR/"data"/"clientes_campanha.csv"
+
+    # Returns:
+    #     DataFrame com os dados
+    
     # TODO 1: Use pd.read_csv() para carregar o arquivo
     # Dica: df = pd.read_csv(caminho_arquivo)
     
-    df = None  # Substitua None pelo código correto
-    
+    df = pd.read_csv(caminho_arquivo)
     return df
-
 
 def explorar_dados(df):
     """
@@ -36,16 +36,29 @@ def explorar_dados(df):
     
     # TODO 2: Mostre o shape do DataFrame (linhas, colunas)
     # Dica: print(f"Shape: {df.shape}")
+    print(f"Shape: {df.shape}")
+    print(f"O arquivo tem {df.shape[0]:,} linhas")
+    print(f"O arquivo tem {df.shape[1]:,} colunas")
     
-    
+    print()
     # TODO 3: Mostre os tipos de cada coluna
     # Dica: print(df.dtypes)
-    
-    
+    print("-" * 50)
+    print("Tipo de dados das colunas:")
+    print("-" * 50)
+    print(df.dtypes)
+
+
+    print()
     # TODO 4: Mostre as 5 primeiras linhas
     # Dica: print(df.head())
     
-    
+    print("-" * 50)
+    print("5 primeiras linhas:")
+    print("-" * 50)
+    print(df.head())
+
+
     print("=" * 50)
 
 
@@ -58,17 +71,23 @@ def verificar_target(df, coluna_target='respondeu_campanha'):
         coluna_target: nome da coluna target
     """
     print("\nDISTRIBUIÇÃO DO TARGET")
-    print("-" * 30)
+    print("-" * 50)
     
     # TODO 5: Mostre a contagem de cada valor do target
     # Dica: print(df[coluna_target].value_counts())
-    
+    print("Contagem de cada valor se respondeu ou não respondeu a campanha;")
+    print(df[coluna_target].value_counts())
+    print()
+    print(f"Quantidade que respondeu a campanha: {df[coluna_target].value_counts().get(1, 0):,}")
+    print(f"Quantidade que não respondeu a campanha: {df[coluna_target].value_counts().get(0, 0):,}")
     
     # TODO 6: Mostre a proporção (percentual) de cada valor
     # Dica: print(df[coluna_target].value_counts(normalize=True))
     
+    print("\nProporção de cada valor do target (%):")
+    print(df[coluna_target].value_counts(normalize=True)*100)
     
-    print("-" * 30)
+    print("-" * 50)
 
 
 # Teste local (executar este arquivo diretamente)
